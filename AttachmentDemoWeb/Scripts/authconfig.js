638eeac66bafda5949ab1da143e9893333a66161
Nick Beaugeard <nick@tribedev.com.au>
2021-04-27 20:58:57 +1000

    Configured to Work

--- AttachmentDemoWeb/Scripts/authconfig.js
+++ AttachmentDemoWeb/Scripts/authconfig.js
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE.txt in the project root for license information.
 var authConfig = {
-    clientId: "Your APP ID Here",
+    clientId: "967ac950-d9d4-4b7a-9fdd-4a2bd0a5d85b",
     scopes: "Files.ReadWrite",
     redirectUrl: "https://localhost:44349/MessageRead.html"
 };
